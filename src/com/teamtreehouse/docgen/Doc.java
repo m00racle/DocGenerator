c7d61582aa9743bd3f7042e84c39521cfdd8850e
mooracle <yanuar.prakosa@gmail.com>
2018-04-15 16:20:43 +0700

    @Doc annotation and DocProcessor class developed

--- src/com/teamtreehouse/docgen/Doc.java
+++ src/com/teamtreehouse/docgen/Doc.java
@@ -1,15 +1,18 @@
 package com.teamtreehouse.docgen;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(value = RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Doc {
     /**
      * Starting the Annotation - Declaration and Retention
      * - we will use Reflection to analyze the presence of our @Doc on classes
      * and methods. In order to use Reflection during the analysis the
-     * @Retention policy must be set to RUNTIME (to ensure this @Doc kept available
+     * Retention policy must be set to RUNTIME (to ensure this @Doc kept available
      * in the JVM runtime)
      *
      * - when we want to declare an interface there are 3 things to do:
@@ -17,7 +20,24 @@ public @interface Doc {
      *   > Specify what are called retention and targets
      *   > Add elements and/or attributes
      *
-     * */
+     * - Next we will specify where this annotation will target to operate
+     *   in our case it will be class (type) and method.
+     *   That is why we use @Target({ElementType.TYPE, ElementType.METHOD})
+     *
+     *   This is will declare elements names as zero parameter non-void methods like below
+     *   we don't have to add public since all interface methods are public by default
+     *   since this is an interface meaning we will define methods with no returns, although
+     *   the dclaration behave more like field which values are set when annotation is added
+     *   to a class or method. Therefore we can specify the default value of each element which
+     *   also saying that the anotation when added to a class or method doesn't have to include
+     *   value for this element*/
 
     //Description of class or method
+   String description() default "";
+
+   //Description of parameters, if annotated lement is a method & has parameters
+    String[] parameters() default {};
+
+    //Description of return value, if annotated element is a method & is not void
+    String returnVal() default "";
 }
--- src/com/teamtreehouse/docgen/DocProcessor.java
+++ src/com/teamtreehouse/docgen/DocProcessor.java
@@ -1,5 +1,8 @@
 package com.teamtreehouse.docgen;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class DocProcessor {
     /**
      * 1. Analyzes the given class's Doc annotation
@@ -23,14 +26,96 @@ public class DocProcessor {
         //Examining if @Doc is appears in the Class declaration:
         //NOTE: it will be better if you set the annotation first before making this if statement:
         if (processedClass.isAnnotationPresent(Doc.class)){
-            //will do more about this but first let me make the else part first
+            //we'll loop over all methods in the processedClass
+            for(Method method : processedClass.getDeclaredMethods()){
+
+                //get method modifiers
+                int modifierInt = method.getModifiers();
+
+                //get method name
+                String methodName = method.getName();
+
+                //check if the method is non-private
+                if(!Modifier.isPrivate(modifierInt)){
+                    //start to check if there is error in @Doc inside all method
+                    int methodErrors = 0;
+
+                    //display the method name
+                    System.out.printf("%n%n\t%s:", methodName);
+
+                    //check if the @Doc is present in the method?
+                    if(method.isAnnotationPresent(Doc.class)){
+                        //if yes get the reference to the actual annotation
+                        Doc doc = method.getAnnotation(Doc.class);
+
+                        //check number of items in parameter description matches the actual parameters
+                        int numMissingParameter = getNumMissingParameters(method, doc);
+                        if(numMissingParameter > 0){
+                            methodErrors++;
+                            System.out.printf("%n\t\t=> Missing %s parameter description(s)", numMissingParameter);
+                        }
+
+                        //check if there is return description if needed
+                        if(!hasReturnDescription(method, doc)){
+                            methodErrors++;
+                            System.out.printf("%n\t\t=> Misiing description of return value");
+                        }
+                    } else {
+                        //non-private method is missing a @Doc annotation
+                        methodErrors++;
+                        System.out.printf("%n\t\t=> @Doc annotation missing");
+                    }
+
+                    //check for azero errors
+                    if(methodErrors == 0){
+                        System.out.printf("%n\t\t=> No changes needed");
+                    }
+
+                    //add method errors to class errors
+                    classError += methodErrors;
+                }
+            }
         } else {
             classError++;
             System.out.printf("%n\t=> Class does not contain the proper documentation");
         }
 
+        //Display final message
+        String yayOrNay = classError == 0 ? "All is Okay" : "Get to documenting";
+        System.out.printf("%n%nDocProcessor has found %s error(s) in class %s. %s!%n",
+                classError, className, yayOrNay);
+
         //return whether there is no class error meaning True, and there is class error meanaing false
         //we use boolean expression on logical equal sign:
         return classError == 0;
+    }
+
+    /**
+     * Determines whether or not a method's return value is missing
+     * @param method Method under consideration
+     * @param doc annotation to check
+     * @return True if method has a void return type or the annotation has non empty return description
+     * */
+    private static boolean hasReturnDescription(Method method, Doc doc) {
+        return method.getReturnType().equals(Void.TYPE) || !doc.returnVal().isEmpty();
+    }
+
+    /**
+     * Check whether or not the number of descriptions provided in the Doc annotation
+     * match the number of parameters in the given method
+     * @param method Method Method under consideration
+     * @param doc Annotation to check
+     * @return Number of description missing.
+     * NOTE: this cpuld be negative if too many description are provided
+     * */
+    private static int getNumMissingParameters(Method method, Doc doc) {
+        int numMissing = 0;
+        int annotatedParameterCount = doc.parameters().length;
+        int actualParameterCount = method.getParameterCount();
+        //to ensure only positive number comes up from  numMissing:
+        if (annotatedParameterCount<actualParameterCount){
+            numMissing = actualParameterCount - annotatedParameterCount;
+        }
+        return numMissing; //if numMissing < 0 then it will return 0
     }
 }
