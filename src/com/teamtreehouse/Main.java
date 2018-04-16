af7ff78fc7cdc5605356e51930ba5d956db74b19
mooracle <yanuar.prakosa@gmail.com>
2018-04-16 10:25:42 +0700

    add main method in Main.java class please check TODO

--- src/com/teamtreehouse/Main.java
+++ src/com/teamtreehouse/Main.java
@@ -1,8 +1,15 @@
 package com.teamtreehouse;
 
+import com.teamtreehouse.docgen.DocProcessor;
+
 public class Main {
 
+    /**
+     * This class holds the main method that initialize the whole program
+     * */
     public static void main(String[] args) {
-	// write your code here
+	// get class object (at the moment Class called MathUtil is not yet available
+        DocProcessor.process(MathUtils.class);
+       //TODO: moo: we will create the MathUtils Class later
     }
 }
