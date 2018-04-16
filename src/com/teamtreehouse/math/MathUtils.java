52b146866671fcf2f68a8dcd5f044676b1cb9d9a
mooracle <yanuar.prakosa@gmail.com>
2018-04-16 11:35:04 +0700

    put together mathUtils, Main and fix all @Doc in MathUtils. All is okay now

--- src/com/teamtreehouse/math/MathUtils.java
+++ src/com/teamtreehouse/math/MathUtils.java
@@ -12,7 +12,7 @@ public class MathUtils {
 
     @Doc(
             description = "Calculate the area of a triangle",
-            parameters = {"Coordinates of the first vertex"},
+            parameters = {"Coordinates of the first vertex", "Coordinate of second vertex", "Third vertex"},
             returnVal = "Calculated area of the triangle"
     )
     public static Double triangleArea(Point2D.Double a, Point2D.Double b, Point2D.Double c) {
@@ -22,12 +22,17 @@ public class MathUtils {
     @Doc(
             description = "Calculate the distance between the given points",
             parameters = {"Coordinates of one point", "Coordinates of another point"},
-            returnVal = ""
+            returnVal = "shortest distance between 2 points"
     )
     public static Double distance(Point2D.Double a, Point2D.Double b) {
         return 0.0;
     }
 
+    @Doc(
+            description = "Calculate quadratic roots of non-linear equation",
+            parameters = {"coeff a", "coeff b", "coeff c"},
+            returnVal = "root of the equation"
+    )
     public static Double[] quadraticRoots(int a, int b, int c) {
         return new Double[]{};
     }
